2e93c53f41c2d5bc8470636c2ae641549860b134
kwthr <k.mahfoodh2002@gmail.com>
2023-11-19 16:35:51 +0300

    fixed the boxes and vertical lines

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function newGame(){
 
     // Set the player to player1
     currentPlayer = document.querySelector(".player1")
-    currentPlayer.classList.add(".playerTurn")
+    currentPlayer.classList.add("playerTurn")
 }
 
 
@@ -32,22 +32,36 @@ function newGame(){
 
 function selectLine(e) {
     e.preventDefault();
-  
+
+    // Targeting the line images and changing them when click
     var clickedImg = e.currentTarget.querySelector('.theLines');
   
     if (clickedImg.getAttribute('src') === 'Img/IMG_0451.PNG') {
       clickedImg.src = 'Img/IMG_0449.PNG';
+
+      // Adding a class to the image to make it appear
       clickedImg.classList.add('theLinesClicked')
     }
+    
+    if (clickedImg.getAttribute('src') === 'Img/IMG_0453.PNG') {
+        clickedImg.src = 'Img/IMG_0454.PNG';
+  
+        // Adding a class to the image to make it appear
+        clickedImg.classList.add('theLinesClicked')
+      }
     
 }
 
 
 
 
-// Attching event listeners to lines 
+// Attching event listeners
 
 document.querySelectorAll(".containingBox a").forEach(function(square){
     square.addEventListener('click',selectLine)
 })
+
+document.querySelector('#DBnewGame').addEventListener('click',newGame);
+
+newGame();
 
